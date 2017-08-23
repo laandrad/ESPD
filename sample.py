12114b98ff5b7887212bcb3ec4ee7eb92a8d5115
Luis Andrade <alejandro@Luiss-MBP.domain>
2017-08-23 14:54:45 -0600

    Mac version of sample.py working

--- sample.py
+++ sample.py
@@ -5,12 +5,15 @@ import sys
 src_dir = os.path.dirname(inspect.getfile(inspect.currentframe()))
 
 # Windows and Linux
-arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
+# arch_dir = '../lib/x64' if sys.maxsize > 2**32 else '../lib/x86'
 
 # Mac
-# arch_dir = os.path.abspath(os.path.join(src_dir, '../lib'))
+arch_dir = os.path.abspath(os.path.join(src_dir, '../ESPD'))
+# arch_dir = os.path.abspath(src_dir)
 
 sys.path.insert(0, os.path.abspath(os.path.join(src_dir, arch_dir)))
+
+print(os.path.abspath(os.path.join(src_dir, arch_dir)))
 
 import Leap
 
